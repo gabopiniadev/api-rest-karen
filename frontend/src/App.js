547e60c3c3cb0e4f1009d0fa634b2fc28ee36110
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-07-07 11:18:57 -0400

    update proyecto react - visualizacion pokemon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,43 @@
-function App() {
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+
+function PokeTable() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=30")
+      .then((response) => {
+        setPokemons(response.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
-    <div className="App">
-      <p>Probando Visualizacion</p>
-    </div>
+    <Table responsive="sm">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Nombre</th>
+          <th>URL</th>
+          <th>Visualizar</th>
+        </tr>
+      </thead>
+      <tbody>
+        {pokemons.map((pokemon, index) => (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{pokemon.name}</td>
+            <td>{pokemon.url}</td>
+            <td><a name="" id="" class="btn btn-primary" href={pokemon.url} role="button">Ver</a></td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   );
 }
 
-export default App;
+export default PokeTable;
