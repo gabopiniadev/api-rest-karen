23d819bb08b0ff39881bc4cd9dc345be23eae9cf
Gabriel Alejandro Piña <gabrielpinia@hotmail.com>
2023-07-07 18:42:58 -0400

    update

--- frontend/src/pages/ViewOne.jsx
+++ frontend/src/pages/ViewOne.jsx
@@ -4,23 +4,20 @@ import Button from "react-bootstrap/Button";
 import { useNavigate } from 'react-router-dom';
 
 function PokeSearch() {
-    // Definir una referencia al campo de texto del formulario
     const inputRef = useRef();
 
-    // Definir una función para manejar el envío del formulario
     const handleSubmit = (event) => {
-        // Prevenir el comportamiento por defecto del formulario
+
         event.preventDefault();
-        // Obtener el valor del campo de texto
+        
         const name = inputRef.current.value;
-        // Redirigir a la ruta que muestra los detalles del pokemon buscado
+
         history.push(`/pokemon/${name}`);
     };
 
-    // Obtener el objeto history de React Router
+    
     const history = useNavigate();
 
-    // Renderizar el componente
     return (
         <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
